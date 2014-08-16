9145eba3101415d258f04bf3857cd9edf68dcb57
Damian Hickey <dhickey@gmail.com>
2014-08-16 11:28:23 +0200

    Task extensions to help alleviate ConfigureAwait(false) eye sores

---
+++ src/NEventStore/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Threading.Tasks
+{
+    using System.Runtime.CompilerServices;
+
+    public static class TaskExtensions
+    {
+        public static ConfiguredTaskAwaitable<T> NotOnOriginalContext<T>(this Task<T> task)
+        {
+            return task.ConfigureAwait(false);
+        }
+
+        public static ConfiguredTaskAwaitable NotOnOriginalContext(this Task task)
+        {
+            return task.ConfigureAwait(false);
+        }
+    }
+}
