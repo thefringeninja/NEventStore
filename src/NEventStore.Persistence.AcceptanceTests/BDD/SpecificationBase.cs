d5bfee8c0c23e3c428f801f32b6abaf5d5b49df5
Damian Hickey <dhickey@gmail.com>
2014-08-16 13:43:24 +0100

    Bit of a crappy way to support async ops in tests.

--- src/NEventStore.Persistence.AcceptanceTests/BDD/SpecificationBase.cs
+++ src/NEventStore.Persistence.AcceptanceTests/BDD/SpecificationBase.cs
@@ -1,18 +1,40 @@
 ﻿namespace NEventStore.Persistence.AcceptanceTests.BDD
 {
+    using System.Threading.Tasks;
     using Xunit;
 
     [RunWith(typeof (SpecificationBaseRunner))]
     public abstract class SpecificationBase
     {
         protected virtual void Because()
-        {}
+        {
+            BecauseAsync().Wait();
+        }
+
+        protected virtual Task BecauseAsync()
+        {
+            return Task.FromResult(0);
+        }
 
         protected virtual void Cleanup()
-        {}
+        {
+            CleanupAsync().Wait();
+        }
+
+        protected virtual Task CleanupAsync()
+        {
+            return Task.FromResult(0);
+        }
 
         protected virtual void Context()
-        {}
+        {
+            ContextAsync().Wait();
+        }
+
+        protected virtual Task ContextAsync()
+        {
+            return Task.FromResult(0);
+        }
 
         public void OnFinish()
         {
