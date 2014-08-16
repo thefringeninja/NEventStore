462f8cd62232b317b450a14c13d943ce73b9bd2a
Damian Hickey <dhickey@gmail.com>
2014-08-16 12:06:18 +0200

    Helper to async invoke a method and capture the exception

--- src/NEventStore.Persistence.AcceptanceTests/Catch.cs
+++ src/NEventStore.Persistence.AcceptanceTests/Catch.cs
@@ -1,6 +1,7 @@
 ﻿namespace NEventStore.Persistence.AcceptanceTests
 {
     using System;
+    using System.Threading.Tasks;
 
     public static class Catch
     {
@@ -9,6 +10,20 @@
             try
             {
                 action();
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+
+            return null;
+        }
+
+        public static async Task<Exception> Exception(Func<Task> action)
+        {
+            try
+            {
+                await action();
             }
             catch (Exception ex)
             {
